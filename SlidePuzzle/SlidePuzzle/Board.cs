c6c04e422d290aab0644ea7d6c95118d999bbe5c
Ben Villalobos <villalobosb93@gmail.com>
2017-04-02 11:20:23 -0700

    Fixed "front end" and complete logic.

--- SlidePuzzle/SlidePuzzle/Board.cs
+++ SlidePuzzle/SlidePuzzle/Board.cs
@@ -29,7 +29,7 @@ namespace SlidePuzzle
     {
         int[,] _board;
         Random _random = new Random();
-        Index emptySpot = new Index();
+        Index _emptySpot = new Index();
 
         public int Width
         {
@@ -48,6 +48,11 @@ namespace SlidePuzzle
         }
 
         public Board(int width, int height)
+        {
+            InitializeBoard(width, height);
+        }
+
+        public void Reset(int width, int height)
         {
             InitializeBoard(width, height);
         }
@@ -78,11 +83,11 @@ namespace SlidePuzzle
 
             if(_board[r1, c1] == -1)
             {
-                emptySpot.Set(r1, c1);
+                _emptySpot.Set(r1, c1);
             }
             else if(_board[r2, c2] == -1)
             {
-                emptySpot.Set(r2, c2);
+                _emptySpot.Set(r2, c2);
             }
         }
 
@@ -98,7 +103,29 @@ namespace SlidePuzzle
         {
             get
             {
-                return emptySpot.Row == Width - 1 && emptySpot.Col == Height;
+                int lastNum = int.MinValue;
+                //if the empty spot isn't at the bottom right corner, we automatically it isn't solved
+                if(_emptySpot.Row != Width -1 || _emptySpot.Col != Height-1)
+                {
+                    return false;
+                }
+                //count through each index. Must be in ascending order until the end
+                for (int row = 0; row < Height; row++)
+                {
+                    for (int col = 0; col < Width; col++)
+                    {
+                        if(row == Width - 1 && col == Height - 1)
+                        {
+                            break;
+                        }
+                        if(_board[row, col] < lastNum)
+                        {
+                            return false;
+                        }
+                        lastNum = _board[row, col];
+                    }
+                }
+                return true;
             }
         }
 
@@ -108,7 +135,7 @@ namespace SlidePuzzle
             {
                 for (int col = 0; col < Width; col++)
                 {
-                    if(row == emptySpot.Row && col == emptySpot.Col)
+                    if(row == _emptySpot.Row && col == _emptySpot.Col)
                     {
                         Console.Write("|    ");
                         if (col == Width - 1)
@@ -137,37 +164,37 @@ namespace SlidePuzzle
         {
             switch (dir)
             {
-                case Direction.Up:
-                    if(emptySpot.Row == Height-1)
-                    {
-                        //can't move
-                        return;
-                    }
-                    SwapOnce(emptySpot.Row, emptySpot.Col, emptySpot.Row + 1, emptySpot.Col);
-                    break;
                 case Direction.Down:
-                    if (emptySpot.Row == 0)
+                    if(_emptySpot.Row == Height-1)
                     {
                         //can't move
                         return;
                     }
-                    SwapOnce(emptySpot.Row, emptySpot.Col, emptySpot.Row - 1, emptySpot.Col);
+                    SwapOnce(_emptySpot.Row, _emptySpot.Col, _emptySpot.Row + 1, _emptySpot.Col);
                     break;
-                case Direction.Left:
-                    if (emptySpot.Col == Width - 1)
+                case Direction.Up:
+                    if (_emptySpot.Row == 0)
                     {
                         //can't move
                         return;
                     }
-                    SwapOnce(emptySpot.Row, emptySpot.Col, emptySpot.Row, emptySpot.Col + 1);
+                    SwapOnce(_emptySpot.Row, _emptySpot.Col, _emptySpot.Row - 1, _emptySpot.Col);
                     break;
                 case Direction.Right:
-                    if (emptySpot.Col == 0)
+                    if (_emptySpot.Col == Width - 1)
                     {
                         //can't move
                         return;
                     }
-                    SwapOnce(emptySpot.Row, emptySpot.Col, emptySpot.Row, emptySpot.Col - 1);
+                    SwapOnce(_emptySpot.Row, _emptySpot.Col, _emptySpot.Row, _emptySpot.Col + 1);
+                    break;
+                case Direction.Left:
+                    if (_emptySpot.Col == 0)
+                    {
+                        //can't move
+                        return;
+                    }
+                    SwapOnce(_emptySpot.Row, _emptySpot.Col, _emptySpot.Row, _emptySpot.Col - 1);
                     break;
                 default:
                     break;
--- SlidePuzzle/SlidePuzzle/Program.cs
+++ SlidePuzzle/SlidePuzzle/Program.cs
@@ -10,12 +10,12 @@ namespace SlidePuzzle
     {
         static void Main(string[] args)
         {
-            Board board = new Board(5, 5);
+            Board board = new Board(4, 4);
             board.SwapRandom(50);
-            
-            while(!board.IsCorrect)
+            board.ShowBoard();
+            while (!board.IsCorrect)
             {
-                board.ShowBoard();
+
                 switch (Console.ReadKey().Key)
                 {
                     case ConsoleKey.LeftArrow:
@@ -30,9 +30,15 @@ namespace SlidePuzzle
                     case ConsoleKey.DownArrow:
                         board.MakeMove(Direction.Down);
                         break;
+                    case ConsoleKey.R:
+                        board.Reset(4, 4);
+                        break;
                 }
                 Console.Clear();
+                board.ShowBoard();
             }
+            Console.WriteLine("Complete!");
+            Console.ReadKey();
         }
     }
 }
